abf77c72d1f4f18ba3675c690dfcce187d38d48e
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-05-23 09:22:26 +0200

    fix: add pagetransitions to index + intro page

--- src/app/[locale]/ervaringsplein/page.tsx
+++ src/app/[locale]/ervaringsplein/page.tsx
@@ -1,21 +1,22 @@
 import Intro from "@/app/components/intro/intro";
 import React from "react";
 import { useTranslations } from "next-intl";
-
+import PageTransition from "@/app/components/transitions/page-transition";
 
 const Ervaringsplein = () => {
   const t = useTranslations("welkom-pagina");
   return (
     <>
-      <Intro 
-       title={t("title")}
-       heading={t("heading")}
-       paragraaf={t("paragraaf")}
-       finalText={t("finalText")}
-       button={t("button")}
-       />
+      <PageTransition>
+        <Intro
+          title={t("title")}
+          heading={t("heading")}
+          paragraaf={t("paragraaf")}
+          finalText={t("finalText")}
+          button={t("button")}
+        />
+      </PageTransition>
     </>
-
   );
 };
 
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,29 +1,32 @@
 import { useTranslations } from "next-intl";
 import Hero from "../components/index/hero/hero";
+import PageTransition from "../components/transitions/page-transition";
 
 export default function Home() {
   const t = useTranslations("home-page");
   return (
     <>
-      <Hero
-        title={t("title")}
-        description={t("description")}
-        branches={{
-          government: t("branches.government"),
-          businesses: t("branches.businesses"),
-          suppliers: t("branches.suppliers"),
-        }}
-        newButtonText={t("new-button")}
-        ctaButtonText={t("cta-button")}
-        demoText={t("demo-text")}
-        newLabel={t("new-label")}
-        features={{
-          wcag: t.raw("features.wcag"),
-          report: t.raw("features.report"),
-          certification: t.raw("features.certification"),
-          eaa: t.raw("features.eaa"),
-        }}
-      />
+      <PageTransition>
+        <Hero
+          title={t("title")}
+          description={t("description")}
+          branches={{
+            government: t("branches.government"),
+            businesses: t("branches.businesses"),
+            suppliers: t("branches.suppliers"),
+          }}
+          newButtonText={t("new-button")}
+          ctaButtonText={t("cta-button")}
+          demoText={t("demo-text")}
+          newLabel={t("new-label")}
+          features={{
+            wcag: t.raw("features.wcag"),
+            report: t.raw("features.report"),
+            certification: t.raw("features.certification"),
+            eaa: t.raw("features.eaa"),
+          }}
+        />
+      </PageTransition>
     </>
   );
 }
